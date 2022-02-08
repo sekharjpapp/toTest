8ade3dd0343424b0f745c96835cbace9006eecb8
Shekhar <Shekhar@LAPTOP-C1P9D84G>
2022-02-08 08:35:12 +0530

    test

--- src/main/java/com/pixeleclipse/App.java
+++ src/main/java/com/pixeleclipse/App.java
@@ -3,7 +3,6 @@ package com.pixeleclipse;
 
 public class App {
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
         System.out.println( "Hello Git World!" );
     }
 }
